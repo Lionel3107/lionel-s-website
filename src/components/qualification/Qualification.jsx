e1aace4d9e89fffd360f4827545ea1faaeab94ab
lionel3107 <dlmsonelterror@gmail.com>
2024-05-09 13:20:24 +0000

    Add qualifications

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,31 +1,154 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./qualification.css";
 
 const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
   return (
-    <section className="qualificaton section">
+    <section className="qualificaton section" id=''>
         <h2 className="section__title">Qualificaton</h2>
-        <span className='section__subtitle'>My Personnal Journey</span>
+        <span className='section__subtitle'>My personel Journey</span>
 
 
         <div className="qualification__container container">
             <div className="qualification__tabs">
-                <div className="qualification__button qualification__active button--flex">
-                    <i className="uil uil-graduation-cap qualification__icon"></i> Education
+                <div className={toggleState === 1 ? "qualification__button qualification__active button--flex" : "qualification__button qualification__active button--flex"} onClick={() =>toggleTab(1)}>
+                    <i className="uil uil-graduation-cap qualification__icon"></i>{" "} Education
                 </div>
 
-                <div className="qualification__button button--flex">
-                    <i className="uil uil-briefcase-alt qualification__icon"></i> Experience
+                <div className={toggleState === 2 ? "qualification__button qualification__active button--flex" : "qualification__button qualification__active button--flex"} onClick={() =>toggleTab(2)}>
+                    <i className="uil uil-briefcase-alt qualification__icon"></i>{" "} Experience
                 </div>
             </div>
 
             <div className="qualification__sections">
-                <div className="qualification__content">
+                <div className={toggleState === 1 ? "qualification__content qualification__content-active" : "qualification__content"}>
                     <div className="qualification__data">
                         <div>
-                            <h3 className="qualification__title"></h3>
-                            <span className="qualification__subtitle"></span>
+                            <h3 className="qualification__title">Web Design</h3>
+                            <span className="qualification__subtitle">Burkina Institute of Technology</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt">
+                                </i>2021 - Present
+                            </div>
+                        </div>
 
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification__line"></span>
+                        </div>
+                    </div>
+
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification_line"></span>
+                        </div>
+
+                        <div>
+                            <h3 className="qualification__title">Art Director</h3>
+                            <span className="qualification__subtitle">Burkina Institute of Technology</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt">
+                                </i>2020 - 2021
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Web Development</h3>
+                            <span className="qualification__subtitle">Burkina Institute of Technology</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt">
+                                </i>2018 - 2020
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification_line"></span>
+                        </div>
+                    </div>
+
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification_line"></span>
+                        </div>
+
+                        <div>
+                            <h3 className="qualification__title">UX Expert</h3>
+                            <span className="qualification__subtitle">Burkina Institute of Technology</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt">
+                                </i>2017 - 2018
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Product Designer</h3>
+                            <span className="qualification__subtitle">Microsoft - Spain</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt">
+                                </i>2021 - Present
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification_line"></span>
+                        </div>
+                    </div>
+
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification_line"></span>
+                        </div>
+
+                        <div>
+                            <h3 className="qualification__title">UX Designer</h3>
+                        <span className="qualification__subtitle">Apple Inc - Burkina</span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt">
+                                </i>2020 - 2021
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="qualification__data">
+                        <div>
+                            <h3 className="qualification__title">Web Designer</h3>
+                            <span className="qualification__subtitle">Figma - USA </span>
+                            <div className="qualification__calender">
+                                <i className="uil uil-calender-alt">
+                                </i>2018 - 2020
+                            </div>
+                        </div>
+
+                        <div>
+                            <span className="qualification__rounder"></span>
+                            <span className="qualification_line"></span>
                         </div>
                     </div>
                 </div>
